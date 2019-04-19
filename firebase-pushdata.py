b69d3c2a54fceca88cccaf4c72b1b76a6a274873
mohammad ahsan <40141811+ahsan8244@users.noreply.github.com>
2019-04-19 19:38:17 +0800

    added comments for individual tasks

--- firebase-pushdata.py
+++ firebase-pushdata.py
@@ -1,5 +1,7 @@
 import pyrebase as pb
+import serial
 
+#set up configuration for the firebase app
 config = {
   "apiKey": "AIzaSyAQ7U9x2dSvAJJ4fh7p9rO7KBO1qPp55KE",
   "authDomain": "mylitnesspal.firebaseapp.com",
@@ -8,13 +10,18 @@ config = {
   "serviceAccount": "mylitnesspal-firebase-adminsdk-yrvkq-637f8b32bc.json"
 }
 
+#initialize firebase
 firebase = pb.initialize_app(config)
 
-data = open("data.txt", "r")
-values = []
-for value in data:
-    value.replace('\n', '')
-    values.append(value)
+#read data from serial port and write to text file
+
+
+#read values from file and write to firebase database
+#data = open("data.txt", "r")
+#values = []
+#for value in data:
+#    value.replace('\n', '')
+#    values.append(value)
 db = firebase.database()
 #db.child("Food weights").set(values)
 lmao = db.child("Food weights").get().val()
